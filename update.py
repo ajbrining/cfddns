ad88a9690d9900f62f9eca6c6025b4262f0cf7c2
Drew Brining <54297943+ajbrining@users.noreply.github.com>
2021-04-23 18:56:16 -0400

    use requests to make api calls

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 from yaml import load, FullLoader
 from miniupnpc import UPnP
-
+import requests
 
 # load the configuration file
 with open('config.yaml') as f:
@@ -14,3 +14,16 @@ upnp.discover()
 upnp.selectigd()
 ip = upnp.externalipaddress()
 
+api_url = 'https://api.cloudflare.com/client/v4'
+headers = {'Authorization': 'Bearer ' + config['api_token'],
+           'Content-Type': 'application/json'}
+
+url = '{}/zones/{}/dns_records?name={}'.format(api_url, config['zone_id'], config['hostname'])
+response = requests.get(url, headers=headers).json()
+
+if response['result'][0]['content'] == ip:
+    exit
+
+url = '{}/zones/{}/dns_records/{}'.format(api_url, config['zone_id'], response['result'][0]['id'])
+data = '{{"content": "{}"}}'.format(ip)
+requests.patch(url, headers=headers, data=data)
