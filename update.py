aa367fb05344cb3d19f70c2fdbf2fe648a044565
Drew Brining <54297943+ajbrining@users.noreply.github.com>
2021-04-23 17:23:38 -0400

    load yaml config file

--- update.py
+++ update.py
@@ -1,5 +1,10 @@
+from yaml import load, FullLoader
 from miniupnpc import UPnP
 
+
+# load the configuration file
+with open('config.yaml') as f:
+    config = load(f, Loader=FullLoader)
 
 upnp = UPnP()
 
